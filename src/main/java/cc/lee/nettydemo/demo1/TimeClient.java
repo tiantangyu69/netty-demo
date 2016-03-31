1514d4c58e9efb88997bd90e0e5c6c1191e82219
tiantangyu69 <304663257@qq.com>
2016-03-31 13:40:05 +0800

    demo

---
+++ src/main/java/cc/lee/nettydemo/demo1/TimeClient.java
@@ -0,0 +1,43 @@
+package cc.lee.nettydemo.demo1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by lizhitao on 16-3-31.
+ */
+public class TimeClient {
+    public void connect(int port, String host) throws Exception {
+        // 配置客户端nio线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHander());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8888;
+        new TimeClient().connect(port, "localhost");
+    }
+}
---
+++ src/main/java/cc/lee/nettydemo/demo1/TimeClientHander.java
@@ -0,0 +1,39 @@
+package cc.lee.nettydemo.demo1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by lizhitao on 16-3-31.
+ */
+public class TimeClientHander extends ChannelHandlerAdapter {
+    private final ByteBuf firstMessage;
+
+    public TimeClientHander() {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+
+        String body = new String(req, "UTF-8");
+        System.out.println("Now is:" + body);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
